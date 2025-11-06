a9ba8a0e501d71f23067dc1994614b2efc221e61
rylub <ryanlubell@ymail.com>
2025-11-05 19:02:02 -0800

    Remove leftover backup files

--- starter_code/models/store.py
+++ starter_code/models/store.py
@@ -14,6 +14,7 @@ class StoreModel(db.Model):
 
     def json(self):
         return {
+            'id': self.id,
             'name': self.name,
             'items': [item.json() for item in self.items.all()]
         }
